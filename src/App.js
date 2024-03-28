944255f5ac46a333292d21a32776aee2de065971
ephesi4ns <ephesi4ns623@gmail.com>
2024-03-28 23:18:03 +0900

    Done 30:20

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.Age}</h2>
+    </>
+  );
+};
+
+const App = () => {
+  const isUserLoggedIN = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="Junbeom" lastName="Doe" Age="30" />
+      <Person name="Mary" lastName="Doe" Age="4" />
     </div>
   );
-}
+};
 
 export default App;
