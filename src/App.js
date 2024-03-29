2068f739b9f891440abb019ab07a28f6ff5e1bfd
ephesi4ns <ephesi4ns623@gmail.com>
2024-03-29 11:35:50 +0900

    Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.Age}</h2>
-    </>
-  );
+const API_URL = "http://www.omdbapi.com?apikey=9ab97ff0";
+
+const movie1 = {
+  Title: "Superman, Spiderman or Batman",
+  Year: "2011",
+  imdbID: "tt2084949",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg",
 };
 
 const App = () => {
-  const isUserLoggedIN = true;
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Super");
+  }, []);
 
   return (
-    <div className="App">
-      <Person name="Junbeom" lastName="Doe" Age="30" />
-      <Person name="Mary" lastName="Doe" Age="4" />
-    </div>
+    <>
+      <div className="app">
+        <h1>MovieLand</h1>
+
+        <div className="search">
+          <input
+            placeholder="Search for movies"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <img
+            src={SearchIcon}
+            alt="search"
+            onClick={() => searchMovies(searchTerm)}
+          />
+        </div>
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie1={movie}></MovieCard>
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </>
   );
 };
 
